d24727214f6db51a01b1917eb808527db0e35247
kongdej <kongdejs@gmail.com>
2016-10-17 14:03:39 +0700

    no message

--- python/reporter.py
+++ python/reporter.py
@@ -12,18 +12,26 @@ url = "https://maker.ifttt.com/trigger/%s/with/key/dJaghatn9VW74F57cTVwdj" % eve
 urlThingspeak = "https://api.thingspeak.com/update.json"
 api_key = "MHTC88ZRMETT82P9"
 
+lux=""
+
 microgear.create(gearkey,gearsecret,appid,{'debugmode': True})
 
 def connection():
   print "Now I am connected with netpie"
-  payload = {'Value1': 'Now Pudza BOT is connected.'}
+  payload = {'value1': 'Now Pudza BOT is connected.'}
   r = requests.post(url, data=payload)
 
 def subscription(topic,message):
   print topic+" "+message
-  payload = {'api_key': api_key, 'field1' : str(message)}
-  r = requests.post(urlThingspeak,params=payload)
-  print r.text
+  lux = message
+  y,m,d,h,mi,s,wd,wy,isd=time.gmtime()  
+  if mi == 12 and s == 0:
+    payload = {'value1': 'Light = '+ lux + ' lux'}
+    r = requests.post(url, data=payload)
+  if s%15 == 0:
+    payload = {'api_key': api_key, 'field1' : str(message)}
+    r = requests.post(urlThingspeak,params=payload)
+    print r.text
 
 def disconnect():
   print "disconnect is work"
